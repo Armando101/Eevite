4b32d6c5d2f9710164d5ff6770fe666ca58d0bfb
Armando Rivera <rivera.armando997@gmail.com>
2023-06-02 01:49:11 -0500

    add heroicons components

--- src/Components/ProductDetail/ProductDetail.tsx
+++ src/Components/ProductDetail/ProductDetail.tsx
@@ -1,3 +1,4 @@
+import { XMark } from "../../Icons/XMark";
 import styles from "./ProductDetail.module.css";
 
 export const ProductDetail = () => {
@@ -8,20 +9,7 @@ export const ProductDetail = () => {
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">Detail</h2>
         <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
+          <XMark />
         </div>
       </div>
     </aside>
